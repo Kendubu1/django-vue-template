f0b2aa9baa876db548a2487a4856235cc7c9033d
Kendrick <Kendubu1@users.noreply.github.com>
2019-11-05 17:26:12 -0600

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -2,11 +2,6 @@
 echo "Starting SSH..."
 /usr/sbin/sshd
 
-echo "Setting Up Template"
-yarn install
-pipenv install --dev && pipenv shell
-python manage.py migrate
-
 echo "Starting Dev Server + Gunicorn..."
 yarn build
 gunicorn --bind=0.0.0.0 --timeout 600 backend.wsgi:application
