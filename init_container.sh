079071843d71b23c9eb464fb80c37e3d021e31d1
Kendrick <Kendubu1@users.noreply.github.com>
2019-11-06 11:26:03 -0600

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -5,9 +5,21 @@ echo "Starting SSH..."
 echo "Template Setup"
 yarn install
 python --version
-pipenv install --dev && pipenv shell
+
+echo "Venv Setup"
+#### 
+pipenv install --dev --skip-lock /bin/activate
+pipenv --where
+pipenv --venv
+echo "pip shell"
+$(pipenv --venv)/bin/activate
+pipenv shell
+####
+
 python manage.py migrate
 
 echo "Starting Dev Server + Gunicorn..."
 yarn build
 gunicorn --bind=0.0.0.0 --timeout 600 backend.wsgi:application
+
+
